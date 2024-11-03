687fc8a36699efdd94e05f4b99431cf6dbf1e6c9
Ghosttrio <ghosttrio@naver.com>
2024-11-03 15:53:56 +0900

    .

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,43 @@
 fun main() {
-    println("hello, world")
+//    println("hello, world")
+//
+//    var test = 11
+//    test = 12
+//    print(test)
+//
+//    val test2 = 1
+//    test2 = 33 // Val cannot be reassigned
+//    print(test2)
+
+//    println("s" + 1) // s1
+//    println("s" * 1) // public inline operator fun BigDecimal.times(other: BigDecimal): BigDecimal defined in kotlin
+//    val a = 1.1
+//    val b: Int = 1
+
+//    voidTest()
+//    val shortTest = shortTest()
+//    shortTest()
+
+//    val y: Boolean = false
+//
+//    if (!y) {
+//        println("test")
+//    }
+
+//    val answer = 1
+//    println("$answer")
+//    println("print $1")
+
+//    val condition = true
+//    println("${if (condition) 'a' else 'b'}")
+
+
+    val range1 = 1..10
+    val range2 = 0 until 10
 }
+
+//fun voidTest() {
+//    print("test")
+//}
+
+//fun shortTest() = println("test")
